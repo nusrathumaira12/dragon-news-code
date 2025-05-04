6988a6a3c2c7a6ff4dc1371203820f0a8130bcb2
nusrathumaira12 <nusrathum31@gmail.com>
2025-05-04 11:44:58 +0600

    added newfiles

--- src/components/homelayout/SocialLogin.jsx
+++ src/components/homelayout/SocialLogin.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 const SocialLogin = () => {
     return (
         <div>
             <h2 className='font-bold mb-5'>Login with</h2>
             <div className='space-y-3 '>
-                <button className='btn w-full btn-outline'>Login With Google</button>
-                <button className='btn w-full btn-outline'>Login With Github</button>
+                <button className='btn w-full btn-outline  btn-secondary'>
+                <FcGoogle size={24}/>  Login With Google</button>
+                <button className='btn w-full btn-outline  btn-primary'>
+                <FaGithub size={24}/>   Login With Github</button>
 
             </div>
         </div>
