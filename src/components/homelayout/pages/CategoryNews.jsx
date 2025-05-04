da872552c0bb2c5bc60c0f06cb5263bb7181e750
nusrathumaira12 <nusrathum31@gmail.com>
2025-05-04 15:18:27 +0600

    added and updated files

--- src/components/homelayout/pages/CategoryNews.jsx
+++ src/components/homelayout/pages/CategoryNews.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
+import NewsCard from '../../NewsCard';
 
 const CategoryNews = () => {
-    const {id}= useParams()
-    console.log(id)
+    const {id}= useParams()  //string
+    const data = useLoaderData();
+
+    const [categoryNews, setCategoryNews] = useState([]);
+    useEffect(()=> {
+if(id =="0"){
+    setCategoryNews(data)
+}else if
+    (id == "1"){
+        const filteredNews=data.filter(
+            news => news.others.is_today_pick == true);
+        setCategoryNews( filteredNews)
+    }else{
+        const filteredNews= data.filter(news => news.category_id == id);
+        setCategoryNews(filteredNews)
+    }
+
+    }, [id,data])
+    // console.log(id, data)
+
+ 
     return (
         <div>
-            CategoryNews- {id}
+           <h2 className='font-bold mb-5'> Total 
+            <span className='text-secondary'>
+                {categoryNews.length}</span> 
+                news Found</h2>
+                <div className='grid grid-cols-1 gap-5'>
+
+                    {
+                        categoryNews.map((news) =>
+                            ( <NewsCard key={news.id} news={news}></NewsCard>))
+                    }
+                </div>
         </div>
     );
 };
